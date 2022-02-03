9b63bf5e5c6a1ed28699d07e76c876c22e86fc8e
Paulo Vitor <paulovitor2123@gmail.com>
2022-02-03 01:53:32 -0300

    Adding splashScreen

--- App.tsx
+++ App.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StatusBar, Text, View } from 'react-native';
 import { Home } from './src/Home';
+import SplashScreen from "react-native-splash-screen";
 
 export default function App() {
+  useEffect(() => {
+    SplashScreen.hide();
+  })
   return (
     <>
       <StatusBar barStyle='light-content' backgroundColor={'#121015'} />
--- android/app/src/main/java/com/todo/MainActivity.java
+++ android/app/src/main/java/com/todo/MainActivity.java
@@ -1,6 +1,8 @@
 package com.todo;
 
 import com.facebook.react.ReactActivity;
+import android.os.Bundle;
+import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
 
@@ -8,8 +10,15 @@ public class MainActivity extends ReactActivity {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
+
   @Override
   protected String getMainComponentName() {
     return "todo";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+      SplashScreen.show(this);
+      super.onCreate(savedInstanceState);
   }
 }
