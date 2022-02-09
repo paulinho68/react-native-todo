282ae1f95418355f0d4925be04116cf30241a19c
Paulo Vitor <paulovitor2123@gmail.com>
2022-02-09 01:36:24 -0300

    Apply useMemo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,32 @@
 
-import React from 'react';
+import React, { useMemo } from 'react';
 import { Image, StyleSheet, Text, View } from "react-native";
 import { useTask } from '../Context/useTask';
 
 export function Header() {
     const { tasks } = useTask();
 
+    const textCounter = useMemo(() => {
+        if (tasks.length) {
+
+            return (
+                <Text style={styles.textHeader}>
+                    {'Você tem '}
+                    <Text style={{ fontWeight: '700' }}>
+                        {tasks.length} {`tarefa${tasks.length === 1 ? '' : 's'}`}
+                    </Text>
+                </Text>
+            )
+        } else {
+            return (<Text style={styles.textHeader}>Adicione uma tarefa</Text>)
+        }
+    }, [tasks])
+
     return (
         <View style={styles.header}>
             <View style={styles.contentHeader}>
                 <Image style={styles.logoImage} source={require('../../assets/todo.png')} />
-                {tasks.length ? (
-                    <Text style={styles.textHeader}>
-                        {'Você tem '}
-                        <Text style={{ fontWeight: '700' }}>
-                            {tasks.length} {`tarefa${tasks.length === 1 ? '' : 's'}`}
-                        </Text>
-                    </Text>
-                ) : (
-                    <Text style={styles.textHeader}>Adicione uma tarefa</Text>
-                )}
+                {textCounter}
             </View>
         </View>
     )
